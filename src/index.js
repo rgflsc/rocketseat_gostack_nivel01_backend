62b9c835aa1f53a474ac76eb4e6f64d52a305542
Rodrigo Garcia <rgflsc@gmail.com>
2020-04-08 21:07:28 -0300

    Métodos HTTP
    Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.send({ message: 'Hello World!!!!!' });
+app.get('/projects', (request, response) => {
+  return response.send(['Projeto 01', 'Projeto 02']);
+});
+
+app.post('/projects', (request, response) => {
+  return response.send(['Projeto 01', 'Projeto 02', 'Projeto 03']);
+});
+
+app.put('/projects/:id', (request, response) => {
+  return response.send(['Projeto 04', 'Projeto 02', 'Projeto 03']);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  return response.send(['Projeto 02', 'Projeto 03']);
 });
 
 app.listen(3333, () => {
