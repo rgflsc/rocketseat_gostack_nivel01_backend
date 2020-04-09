33ed3b4c2c1d6d44e181c50ee50aed847c8bd4ee
Rodrigo Garcia <rgflsc@gmail.com>
2020-04-08 21:43:03 -0300

    Tipos de Parâmetros
    Tipos de Parâmetros

--- src/index.js
+++ src/index.js
@@ -2,6 +2,27 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar informações do backend
+ * POST: Criar uma informação no backend
+ * PUT/PATCH: Alterar uma informação no backend
+ * DELETE: Deletar uma informação no backend
+ * 
+ */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Quert params: Filtros e paginação
+ * Route params: Identificar recursos (Atualizar/Deletar)
+ * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
+ * 
+ */
+
 app.get('/projects', (request, response) => {
   return response.send(['Projeto 01', 'Projeto 02']);
 });
