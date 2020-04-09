18da5042c7b6b11bc8569a8e0760f3fed25d9336
Rodrigo Garcia <rgflsc@gmail.com>
2020-04-08 22:30:41 -0300

    Middlewares
    Middlewares

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -24,7 +24,37 @@ app.use(express.json());
  * 
  */
 
+/**
+ * Middleware:
+ * 
+ * Interceptador de requisições que pode interromper totalmente ou alterar dados da requisição.  * 
+ * 
+ */
+
 const projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+
+  return next();
+}
+
+function validadeProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.' });
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validadeProjectId);
 
 app.get('/projects', (request, response) => {
   const { title } = request.query;
@@ -80,4 +110,3 @@ app.delete('/projects/:id', (request, response) => {
 app.listen(3333, () => {
   console.log('Backend started!');
 });
-
